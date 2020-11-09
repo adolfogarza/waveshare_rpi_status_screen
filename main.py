18e9ab59a689ffc7c3cd83fcb260c1a7cf05d09d
Adolfo <adolfo@Adolfos-MacBook-Pro.local>
2020-11-08 20:52:58 -0800

    update font

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ try:
     print("Show time")
     epd.init(epd.PART_UPDATE)    
     epd.Clear(0xFF)
-    font24 = ImageFont.truetype('fonts/Roboto-Black.ttf', 24)
+    font15 = ImageFont.truetype('fonts/arial.ttf', 15)
     time_image = Image.new('1', (epd2in13.EPD_HEIGHT, epd2in13.EPD_WIDTH), 255) # 255: clear the frame
     time_draw = ImageDraw.Draw(time_image)
     while (True):
         time_draw.rectangle((10, 10, 120, 50), fill = 255)
-        time_draw.text((10, 10), time.strftime('%H:%M:%S'), font = font24, fill = 0)
+        time_draw.text((10, 10), time.strftime('%H:%M:%S'), font = font15, fill = 0)
         newimage = time_image.crop([10, 10, 120, 50])
         time_image.paste(newimage, (10,10))  
         epd.displayPartial(epd.getbuffer(time_image))
