b47501827ef4478a6a4e3d4771bffd446f259057
Adolfo <adolfo@Adolfos-MacBook-Pro.local>
2020-11-08 21:58:33 -0800

    update

--- display_internet_data.py
+++ display_internet_data.py
@@ -24,16 +24,21 @@ try:
 	gateway = gw[2]
 	host_ip = s.getsockname()[0]
 	host_name = socket.gethostname() 
+	ssid = os.popen("iwconfig wlan0 \
+				| grep 'ESSID' \
+				| awk '{print $4}' \
+				| awk -F\\\" '{print $2}'").read()
 
 	# Drawing information on diplay
 	try:
-		image = Image.open('/home/pi/.config/autostart/train.bmp') 
+		image = Image.open('/home/pi/.config/autostart/train.bmp')
 	except:
 		image = Image.new('1', (epd2in13.EPD_HEIGHT, epd2in13.EPD_WIDTH), 255)  # 255: clear the frame
 	draw = ImageDraw.Draw(image)
-	draw.text((10, 10), f'host name: {host_name}', font = preferredFont, fill = 0)
-	draw.text((10, 35), f'host ip: {host_ip}', font = preferredFont, fill = 0)
-	draw.text((10, 60), f'gateway: {gateway}', font = preferredFont, fill = 0)
+	draw.text((10, 10), f'ssid: {ssid}', font = preferredFont, fill = 0)
+	draw.text((10, 35), f'host name: {host_name}', font = preferredFont, fill = 0)
+	draw.text((10, 60), f'host ip: {host_ip}', font = preferredFont, fill = 0)
+	draw.text((10, 85), f'gateway: {gateway}', font = preferredFont, fill = 0)
 	epd.display(epd.getbuffer(image.rotate(180)))
 	epd.sleep()
 		
